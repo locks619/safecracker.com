d0820b990b40a07c44435b5dcf702b673983af69
Felix Rivas <felix@example.com>
2025-12-27 18:21:19 -0800

    Deploy dist to gh-pages

--- astro.config.mjs
+++
@@ -1,1 +0,0 @@
-﻿import { defineConfig } from "astro/config";import tailwind from "@astrojs/tailwind";export default defineConfig({integrations: [tailwind()],output: "static",site: "https://safecracker.com"});
