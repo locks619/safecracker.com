56a56d26cb305957b0b3669c21556c2681ab0e5a
Felix Rivas <felix@example.com>
2026-01-03 17:42:01 -0800

    chore: preserve CNAME + .nojekyll after docs build

---
+++ scripts/postbuild-pages.js
@@ -0,0 +1,9 @@
+﻿const fs = require("fs");
+const path = require("path");
+
+const docs = path.join(process.cwd(), "docs");
+if (!fs.existsSync(docs)) process.exit(0);
+
+fs.writeFileSync(path.join(docs, "CNAME"), "safecrackerinc.com");
+fs.writeFileSync(path.join(docs, ".nojekyll"), "");
+console.log("postbuild: wrote docs/CNAME and docs/.nojekyll");
